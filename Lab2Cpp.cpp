9bfb6208fbfdf6b229f0ec178c73cdcb37b9bff4
Gatalski <66117686+GatalskiAnton@users.noreply.github.com>
2021-10-07 17:01:28 +0300

    Update Lab2Cpp.cpp

--- Lab2Cpp.cpp
+++ Lab2Cpp.cpp
@@ -5,24 +5,9 @@
 #define MAX_NUMBER_OF_ARRAY_ELEMENTS 20
 
 using namespace std;
-int main()
-{
-	srand(time(NULL));
-	int number_of_array_elements;
-	int i = 0;
-	do
-	{
-		if (i > 0)
-		{
-			cout << "Incorrect value\n";
-		};
-		cout << "Enter number of array elements (0, " <<  MAX_NUMBER_OF_ARRAY_ELEMENTS << "]" <<  "\n";
-		i++;
-		cin >> number_of_array_elements;
-		
-	} while ((number_of_array_elements > MAX_NUMBER_OF_ARRAY_ELEMENTS) or (number_of_array_elements < 0));
-	int* arr = new int[number_of_array_elements];
-	int left_border,right_border;
+	
+int* EnterArr(int* arr, int number_of_array_elements) {
+	int left_border, right_border;
 	cout << "How do you want to filing the array, by keyboard input or random number generator(type keyboard, random accordingly\n";
 	string filing_method;
 	cin >> filing_method;
@@ -41,7 +26,6 @@ int main()
 		cin >> right_border;
 		for (int i = 0; i < number_of_array_elements; i++)
 		{
-
 			arr[i] = (rand() % right_border + left_border);
 		};
 	};
@@ -49,68 +33,13 @@ int main()
 	{
 		cout << arr[i] << endl;
 	}
-	int composition = 1;
-	int zero_pos1, zero_pos2;
-	for (int i = 0; i < number_of_array_elements; i++)
-	{
-		if (arr[i] == 0) {
-			zero_pos1 = i;
-			
-			break;
-		}
-		else {
-			zero_pos1 = number_of_array_elements + 1;
-		}
-
-	}
-	for (int i = 0; i < number_of_array_elements; i++)
-	{
-		if (arr[i] == 0 and i != zero_pos1)
-		{
-			zero_pos2 = i;
-			break;
-		}
-		else {
-			zero_pos2 = number_of_array_elements + 1;
-		}
-	}
-	if (zero_pos1 > number_of_array_elements or zero_pos2 > number_of_array_elements)
-	{
-		
-		cout << "It is not possible to count the composition\n";
-	}
-	else {
-
-		for (i = (zero_pos1 + 1); i < zero_pos2 ; i++)
-		{
-			composition *= arr[i];
-		};
-		cout << "Composition : " << composition << endl;
-
-	}
-	
-	int number_of_sign_changes = 0;
-	
-	for (int i = 0; i < (number_of_array_elements - 1); i++)
-	{
-
-		if (arr[i] * arr[i + 1] == 0 && (arr[i] == 0 || arr[i + 1] == 0) && (arr[i] < 0 || arr[i + 1] < 0))
-		{
-			++number_of_sign_changes;
-		}
-		if (arr[i] * arr[i + 1] < 0 && ((arr[i] > 0 && arr[i + 1] < 0) || (arr[i] < 0 && arr[i + 1] > 0)))
-		{
-			++number_of_sign_changes;
-		}
-
-
-	}
-	cout << "Number of sign changes : " << number_of_sign_changes
-	<< endl;
+	return arr;
+}
+int SortArr(int* arr, int number_of_array_elements) {
 	for (int i = number_of_array_elements; i > 0; --i)
 	{
 		int j = i - 1;
-		for ( j = number_of_array_elements; j > 0; --j)
+		for (j = number_of_array_elements; j > 0; --j)
 		{
 			if (arr[j] % 2 && arr[j - 1])
 			{
@@ -121,12 +50,82 @@ int main()
 			}
 		}
 	}
-
 	for (int i = 0; i < number_of_array_elements; i++)
 	{
 		cout << arr[i] << endl;
 	};
+	return 0;
 
-	
 }
+int NumberOfSighnChanges(int* arr, int number_of_array_elements) {
+	int number_of_sign_changes = 0;
+	for (int i = 0; i < (number_of_array_elements - 1); i++)
+	{
+		if (arr[i] * arr[i + 1] == 0 && (arr[i] == 0 || arr[i + 1] == 0) && (arr[i] < 0 || arr[i + 1] < 0)) {
+			++number_of_sign_changes;
+		}
+		if (arr[i] * arr[i + 1] < 0 && ((arr[i] > 0 && arr[i + 1] < 0) || (arr[i] < 0 && arr[i + 1] > 0))) {
+			++number_of_sign_changes;
+		}
+	}
+	cout << "Number of sign changes : " << number_of_sign_changes << endl;
+	return 0;
+}
+int Composition(int* arr, int number_of_array_elements) {
+	int composition = 1;
+	int zero_pos1(0), zero_pos2(0);
+	for (int i = 0; i < number_of_array_elements; i++)
+	{
+		if (arr[i] == 0) {
+			zero_pos1 = i;
+			break;
+		}
+		else {
+			zero_pos1 = number_of_array_elements + 1;
+		}
 
+	}
+	for (int i = 0; i < number_of_array_elements; i++)
+	{
+		if (arr[i] == 0 and i != zero_pos1) {
+			zero_pos2 = i;
+			break;
+		}
+		else {
+			zero_pos2 = number_of_array_elements + 1;
+		}
+	}
+	if (zero_pos1 > number_of_array_elements or zero_pos2 > number_of_array_elements) {
+		cout << "It is not possible to count the composition\n";
+	}
+	else {
+		for (int i = (zero_pos1 + 1); i < zero_pos2; i++)
+		{
+			composition *= arr[i];
+		};
+		cout << "Composition : " << composition << endl;
+	}
+	return composition;
+}
+int main()
+{
+	srand(time(NULL));
+	int number_of_array_elements;
+	int i = 0;
+	do
+	{
+		if (i > 0)
+		{
+			cout << "Incorrect value\n";
+		};
+		cout << "Enter number of array elements (0, " << MAX_NUMBER_OF_ARRAY_ELEMENTS << "]" << "\n";
+		i++;
+		cin >> number_of_array_elements;
+
+	} while ((number_of_array_elements > MAX_NUMBER_OF_ARRAY_ELEMENTS) or (number_of_array_elements < 0));
+	int* arr = new int[number_of_array_elements];
+	EnterArr(arr, number_of_array_elements);
+	Composition(arr, number_of_array_elements);
+	NumberOfSighnChanges(arr, number_of_array_elements);
+	SortArr(arr, number_of_array_elements);
+}
